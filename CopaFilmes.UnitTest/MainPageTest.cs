34f016a4d9d322fb0f5c3114fa8047c29c033afe
Kainã Almeida <kainafa@hotmail.com>
2018-10-23 15:00:01 -0300

    KA - Refactor para uso de styles na MainPage

---
+++ CopaFilmes.UnitTest/MainPageTest.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CopaFilmes.ViewModels;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CopaFilmes.UnitTest
+{
+    [TestClass]
+    public class MainPageTest
+    {
+
+        
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+            
+
+        }
+    }
+}
---
+++ CopaFilmes.UnitTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("CopaFilmes.UnitTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("CopaFilmes.UnitTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("038c9472-5159-49d3-a884-a02633788c35")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
